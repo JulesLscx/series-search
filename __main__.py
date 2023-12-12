fc61b8dde928ded2c6c1b66b8b06b5d1d9c43441
Jules <jlslascoux@gmail.com>
2023-12-12 12:14:11 +0100

    change in main.py

--- __main__.py
+++ __main__.py
@@ -25,6 +25,12 @@ def get_help():
     recommand: Run the recommendation test with a static serie list (see __main__.py for the list)
     """
     print(help_str)
+def run_tests():
+    retcode = pytest.main(["-x", "./test", "--disable-warnings"])
+    if retcode != 0:
+        print("Error in tests. Tests must pass before running the API.")
+        exit()
+
 def treat_import_command():
     # if not data in ./data/processed, raise error
     if(os.path.exists('./data/processed') and os.listdir('./data/processed') == []):
@@ -68,56 +74,52 @@ def launch_api():
     from api.app import create_app
     app = create_app()
     app.run(debug=True, host='')
-# If command is process, get the zip and process it, store it in data/processed
-if len(sys.argv) < 2:
-    print("Please specify a command")
-    get_help()
     exit()
-elif sys.argv[1] == "help" or sys.argv[1] == "-h" or sys.argv[1] == "--help":
-    get_help()
-    exit()
-if sys.argv[1] == "unzip":
-    print("Called unzip command")
-    if len(sys.argv) < 3:
-        print("Please specify a path to unzip")
+    
+def main():
+    if len(sys.argv) < 2:
+        print("Please specify a command")
+        get_help()
         exit()
-    treat_unzip_command(sys.argv[2])
-    exit()
-elif sys.argv[1] == "import":
-    treat_import_command()
-    exit()
-elif sys.argv[1] == "query":
-    #Must have a text to query
-    if len(sys.argv) < 3:
-        print("Please specify a text to query")
+    elif sys.argv[1] == "help" or sys.argv[1] == "-h" or sys.argv[1] == "--help":
+        get_help()
         exit()
-    text = sys.argv[2]
-    treated_query_command(text)
-    exit()
-elif sys.argv[1] == "score_search":
-    es = Elasticsearch([ES_ENDPOINT], basic_auth=(ES_USER, ES_PASSWORD) , verify_certs=False, ssl_show_warn=False)
-    # test_search.run(es)
-    test_search(es)
-    exit()
-elif sys.argv[1] == "recommand":
-    es = Elasticsearch([ES_ENDPOINT], basic_auth=(ES_USER, ES_PASSWORD) , verify_certs=False, ssl_show_warn=False)
-    from es_interface.recommendation import recommendate
-    print(recommendate(es, ["alias", "ncis", "24", "breakingbad", "prisonbreak",'smallville', 'stargatesg1', 'friends', 'scrubs', 'charmed', 'southpark', 'bones', 'xfiles', 'onetreehill', 'lost','criminalminds', 'entourage', 'buffy', 'coldcase', 'supernatural', 'desperatehousewives', 'greysanatomy', 'doctorwho', 'intreatment', 'theoc','howimetyourmother', 'uglybetty', 'angel', 'ghostwhisperer', 'medium', 'thesopranos', 'niptuck', 'thepretender', 'veronicamars', 'weeds']))
-    exit()
-elif sys.argv[1] == "run_api":
-    launch_api()
-elif sys.argv[1] == "test_api":
-    retcode = pytest.main(["-x", "./test", "--disable-warnings"])
-    if retcode != 0:
-        print("Error in tests, don't run the api")
+    if sys.argv[1] == "unzip":
+        print("Called unzip command")
+        if len(sys.argv) < 3:
+            print("Please specify a path to unzip")
+            exit()
+        treat_unzip_command(sys.argv[2])
+        exit()
+    elif sys.argv[1] == "import":
+        treat_import_command()
+        exit()
+    elif sys.argv[1] == "query":
+        #Must have a text to query
+        if len(sys.argv) < 3:
+            print("Please specify a text to query")
+            exit()
+        text = sys.argv[2]
+        treated_query_command(text)
+        exit()
+    elif sys.argv[1] == "score_search":
+        es = Elasticsearch([ES_ENDPOINT], basic_auth=(ES_USER, ES_PASSWORD) , verify_certs=False, ssl_show_warn=False)
+        # test_search.run(es)
+        test_search(es)
+        exit()
+    elif sys.argv[1] == "recommand":
+        es = Elasticsearch([ES_ENDPOINT], basic_auth=(ES_USER, ES_PASSWORD) , verify_certs=False, ssl_show_warn=False)
+        from es_interface.recommendation import recommendate
+        print(recommendate(es, ["alias", "ncis", "24", "breakingbad", "prisonbreak",'smallville', 'stargatesg1', 'friends', 'scrubs', 'charmed', 'southpark', 'bones', 'xfiles', 'onetreehill', 'lost','criminalminds', 'entourage', 'buffy', 'coldcase', 'supernatural', 'desperatehousewives', 'greysanatomy', 'doctorwho', 'intreatment', 'theoc','howimetyourmother', 'uglybetty', 'angel', 'ghostwhisperer', 'medium', 'thesopranos', 'niptuck', 'thepretender', 'veronicamars', 'weeds']))
+        exit()
+    elif sys.argv[1] == "test_api":
+        run_tests()
+        exit()
+    elif sys.argv[1] == "run_api":
+        launch_api()
     else:
-        print("Tests passed, you can run the api")
-        print("Do you want to run the api ? (y/n)")
-        answer = input()
-        if answer == "y":
-            launch_api()
-    exit()
-else:
-    print("Unknown command")
-    get_help()
-    exit()
+        print("Unknown command")
+        get_help()
+        exit()
+if __name__ == "__main__":
+    main()
